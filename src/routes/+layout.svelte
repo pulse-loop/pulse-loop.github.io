e1a67ecc4bef64c42a886e7d11ea929bc4944f65
Riccardo Persello <riccardo.persello@icloud.com>
2023-05-06 14:46:39 +0200

    Sidebar test.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,21 @@
 	import '../app.css';
 </script>
 
-<slot />
+<!-- Vertical sidebar -->
+<nav
+	class="h-16 rotate-90 origin-top-left translate-x-16 bg-white flex flex-row items-center absolute"
+>
+	<!-- Padding -->
+	<div class="flex-grow" />
+	<img src="logo.svg" alt="pulse.loop logo" class="h-16 rotate-180" />
+</nav>
+
+<main class="bg-red-50 max-w-6xl px-32">
+	<slot />
+</main>
+
+<style>
+	nav {
+		width: 100vh;
+	}
+</style>
